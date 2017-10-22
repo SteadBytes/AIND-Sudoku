279c92ac62f24f5c30928fc242f5bed0c806e73b
Ben Steadman <=>
2017-10-22 16:31:50 +0100

    test eliminate()

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -2,6 +2,7 @@ import solution
 import unittest
 import json
 import os
+
 TEST_DATA_PATH = os.path.join(os.path.dirname(__file__), 'test_data.json')
 with open(TEST_DATA_PATH) as f:
     test_data = json.load(f)
@@ -21,6 +22,23 @@ class TestNakedTwins(unittest.TestCase):
     def test_naked_twins2(self):
         self.assertTrue(solution.naked_twins(self.before_naked_twins_2) in self.possible_solutions_2,
                         "Your naked_twins function produced an unexpected board.")
+
+
+class TestEliminate(unittest.TestCase):
+    values_before = test_data['basic_puzzle']['unsolved_dict']
+    initial_solved = test_data['basic_puzzle']['initial_solved_boxes']
+    peers = test_data['general']['peers']
+
+    def test_eliminate(self):
+        """Test that the value of each solved box is removed from the values of
+        all its peers
+        """
+        values_after = solution.eliminate(self.values_before)
+        for box in self.initial_solved:
+            peer_vals = []
+            for p in self.peers[box]:
+                peer_vals += values_after[p]
+        self.assertNotIn(values_after[box], peer_vals)
 
 
 class TestDiagonalSudoku(unittest.TestCase):
